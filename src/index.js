ab2dc88fd4edb89d4d75839c3017fbdf39a8772c
vitalur <39513998+vitalur@users.noreply.github.com>
2018-09-20 07:24:03 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	
+	let bank = {};
+	if (currency < 0) {
+		return bank;
+	} else if (currency >= 10000) {
+		return  {error: "You are rich, my friend! We don't have so much coins for exchange"};
+	}    
+
+	
+	const money = ['50', '25', '10', '5', '1'];
+	for (var i = 0; i < money.length; i++) {
+		var count = 0;
+		while (currency >= money[i]) {
+			currency -= money[i];
+			count++;
+		}
+		if (count > 0) {
+			switch(money[i]) {
+				case '50': bank.H = count;  break;
+				case '25': bank.Q = count;  break;
+				case '10': bank.D = count;  break;
+				case '5': bank.N = count;  break;
+				case '1': bank.P = count;  break;
+				default: break;
+			}
+		}
+	}
+	return bank;
 }
+
+//return for(var p in bank) => (p + ' : ' +bank[p]);
